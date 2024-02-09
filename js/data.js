35d7091c9b33c1574d1ed0fccdf9b1f726f77947
grace-truthseeker <65409204+grace-site@users.noreply.github.com>
2024-02-09 17:46:17 -0500

    Added pages for corprio website and icons

--- js/data.js
+++ js/data.js
@@ -1,176 +1,221 @@
 //projects array stores values for each project item's name, href, image link, and genre
 const projects = [
-
-    ["Fallen fate | fateful fall",
-        "/projects/fallen-fate-fateful-fall.html",
-        "/images/icons/fallen00.jpg",
-        "installation"],
-
-    ["Seek",
-        "/projects/seek.html",
-        "/images/icons/seek00.png",
-        "coding"],
-
-    ["Ignorance of Idolatrous Knowledge",
-        "/projects/ignorance-of-idolatrous-knowledge.html",
-        "/images/icons/ignorance00.png",
-        "coding"],
-
-    ["Blue Blooms",
-        "/projects/blue-blooms.html",
-        "/images/icons/blue00.jpg",
-        "fine-art"],
-
-    ["Data in Art Dimension",
-        "/projects/data-in-art-dimension.html",
-        "/images/icons/data00.jpg",
-        "installation"],
-
-    ["Beckett",
-        "/projects/beckett.html",
-        "/images/icons/beckett00.jpg",
-        "fine-art"],
-
-    ["Cinderella",
-        "/projects/cinderella.html",
-        "/images/icons/cinderella00.jpg",
-        "coding"],
-
-    ["Comb in ati on",
-        "/projects/combination.html",
-        "/images/icons/combination00.jpg",
-        "coding"],
-
-    ["Countdown to Heaven",
-        "/projects/countdown-to-heaven.html",
-        "/images/icons/countdown00.jpg",
-        "installation"],
-
-    ["Nostalgic Moon",
-        "/projects/nostalgic-moon.html",
-        "/images/icons/nostalgic00.jpg",
-        "animation"],
-
-    ["Serialist Origami",
-        "/projects/serialist-origami.html",
-        "/images/icons/serialist00.jpg",
-        "installation"],
-
-    ["Eeyores",
-        "/projects/eeyores.html",
-        "/images/icons/eeyores00.png",
-        "animation"],
-
-    ["eis",
-        "/projects/eis.html",
-        "/images/icons/eis00.png",
-        "animation"],
-
-    ["A Dream Come True",
-        "/projects/a-dream-come-true.html",
-        "/images/icons/dream00.jpg",
-        "animation"],
-
-    ["Claw Machine",
-        "/projects/claw-machine.html",
-        "/images/icons/claw00.png",
-        "coding"],
-
-    ["Switched Match",
-        "/projects/switched-match.html",
-        "/images/icons/switched00.png",
-        "coding"],
-
-    ["Individualist Chess",
-        "/projects/individualist-chess.html",
-        "/images/icons/individualist00.png",
-        "coding"],
-
-    ["Tong",
-        "/projects/tong.html",
-        "/images/icons/tong00.jpg",
-        "animation"],
-
-    ["Mr. Mould the Moose",
-        "/projects/mr-mould-the-moose.html",
-        "/images/icons/moose00.jpg",
-        "fine-art"],
-
-    ["Venice",
-        "/projects/venice.html",
-        "/images/icons/venice00.jpg",
-        "fine-art"],
-
-    ["For Aunt Muriel",
-        "/projects/for-aunt-muriel.html",
-        "/images/icons/muriel00.jpg",
-        "fine-art"],
-
-    ["Reflection",
-        "/projects/reflection.html",
-        "/images/icons/reflection00.jpg",
-        "fine-art"],
-
-    ["Eve's Bite",
-        "/projects/eves-bite.html",
-        "/images/icons/eves00.png",
-        "animation"],
-
-    ["Book Illustrations",
-        "/projects/book-illustrations.html",
-        "/images/icons/book00.png",
-        "fine-art"],
-
-    ["Wedding Invitation",
-        "/projects/wedding-invitation.html",
-        "/images/icons/wedding00.jpg",
-        "graphic-design"],
-
-    ["Idea Campaign",
-        "/projects/idea-campaign.html",
-        "/images/icons/idea00.png",
-        "graphic-design"],
-
-    ["Inclusion Ambassadors",
-        "/projects/inclusion-ambassadors.html",
-        "/images/icons/ia00.png",
-        "graphic-design"],
-
-    ["Swift to Hear",
-        "/projects/swift-to-hear.html",
-        "/images/icons/swift00.png",
-        "graphic-design"],
-
-    ["IA Logo",
-        "/projects/ia-logo.html",
-        "/images/icons/logo00.png",
-        "graphic-design"],
-
-    ["Clockbeat",
-        "/projects/clockbeat.html",
-        "/images/icons/clockbeat00.jpg",
-        "installation"],
-
-    ["Hole in the Ground",
-        "/projects/hole-in-the-ground.html",
-        "/images/icons/hole00.jpg",
-        "animation"],
-
-    ["Shower Bed",
-        "/projects/shower-bed.html",
-        "/images/icons/shower00.jpg",
-        "animation"],
-
-    ["Bouncy Cylinder",
-        "/projects/bouncy-cylinder.html",
-        "/images/icons/bouncy00.jpg",
-        "animation"],
-
-    ["This website",
-        "/projects/this-website.html",
-        "/images/icons/website00.png",
-        "coding"]
-
+    {
+        name: "Fallen fate | fateful fall",
+        url: "/projects/fallen-fate-fateful-fall.html",
+        iconUrl: "/images/icons/fallen00.jpg",
+        genre: "installation"
+    },
+    {
+        name: "Seek",
+        url: "/projects/seek.html",
+        iconUrl: "/images/icons/seek00.png",
+        genre: "coding"
+    },
+    {
+        name: "Ignorance of Idolatrous Knowledge",
+        url: "/projects/ignorance-of-idolatrous-knowledge.html",
+        iconUrl: "/images/icons/ignorance00.png",
+        genre: "coding"
+    },
+    {
+        name: "Blue Blooms",
+        url: "/projects/blue-blooms.html",
+        iconUrl: "/images/icons/blue00.jpg",
+        genre: "fine-art"
+    },
+    {
+        name: "Data in Art Dimension",
+        url: "/projects/data-in-art-dimension.html",
+        iconUrl: "/images/icons/data00.jpg",
+        genre: "installation"
+    },
+    {
+        name: "Beckett",
+        url: "/projects/beckett.html",
+        iconUrl: "/images/icons/beckett00.jpg",
+        genre: "fine-art"
+    },
+    {
+        name: "Cinderella",
+        url: "/projects/cinderella.html",
+        iconUrl: "/images/icons/cinderella00.jpg",
+        genre: "coding"
+    },
+    {
+        name: "Comb in ati on",
+        url: "/projects/combination.html",
+        iconUrl: "/images/icons/combination00.jpg",
+        genre: "coding"
+    },
+    {
+        name: "Countdown to Heaven",
+        url: "/projects/countdown-to-heaven.html",
+        iconUrl: "/images/icons/countdown00.jpg",
+        genre: "installation"
+    },
+    {
+        name: "Nostalgic Moon",
+        url: "/projects/nostalgic-moon.html",
+        iconUrl: "/images/icons/nostalgic00.jpg",
+        genre: "animation"
+    },
+    {
+        name: "Serialist Origami",
+        url: "/projects/serialist-origami.html",
+        iconUrl: "/images/icons/serialist00.jpg",
+        genre: "installation"
+    },
+    {
+        name: "Eeyores",
+        url: "/projects/eeyores.html",
+        iconUrl: "/images/icons/eeyores00.png",
+        genre: "animation"
+    },
+    {
+        name: "eis",
+        url: "/projects/eis.html",
+        iconUrl: "/images/icons/eis00.png",
+        genre: "animation"
+    },
+    {
+        name: "A Dream Come True",
+        url: "/projects/a-dream-come-true.html",
+        iconUrl: "/images/icons/dream00.jpg",
+        genre: "animation"
+    },
+    {
+        name: "Claw Machine",
+        url: "/projects/claw-machine.html",
+        iconUrl: "/images/icons/claw00.png",
+        genre: "coding"
+    },
+    {
+        name: "Switched Match",
+        url: "/projects/switched-match.html",
+        iconUrl: "/images/icons/switched00.png",
+        genre: "coding"
+    },
+    {
+        name: "Individualist Chess",
+        url: "/projects/individualist-chess.html",
+        iconUrl: "/images/icons/individualist00.png",
+        genre: "coding"
+    },
+    {
+        name: "Tong",
+        url: "/projects/tong.html",
+        iconUrl: "/images/icons/tong00.jpg",
+        genre: "animation"
+    },
+    {
+        name: "Mr. Mould the Moose",
+        url: "/projects/mr-mould-the-moose.html",
+        iconUrl: "/images/icons/moose00.jpg",
+        genre: "fine-art"
+    },
+    {
+        name: "Venice",
+        url: "/projects/venice.html",
+        iconUrl: "/images/icons/venice00.jpg",
+        genre: "fine-art"
+    },
+    {
+        name: "For Aunt Muriel",
+        url: "/projects/for-aunt-muriel.html",
+        iconUrl: "/images/icons/muriel00.jpg",
+        genre: "fine-art"
+    },
+    {
+        name: "Reflection",
+        url: "/projects/reflection.html",
+        iconUrl: "/images/icons/reflection00.jpg",
+        genre: "fine-art"
+    },
+    {
+        name: "Eve's Bite",
+        url: "/projects/eves-bite.html",
+        iconUrl: "/images/icons/eves00.png",
+        genre: "animation"
+    },
+    {
+        name: "Book Illustrations",
+        url: "/projects/book-illustrations.html",
+        iconUrl: "/images/icons/book00.png",
+        genre: "fine-art"
+    },
+    {
+        name: "Wedding Invitation",
+        url: "/projects/wedding-invitation.html",
+        iconUrl: "/images/icons/wedding00.jpg",
+        genre: "graphic-design"
+    },
+    {
+        name: "Idea Campaign",
+        url: "/projects/idea-campaign.html",
+        iconUrl: "/images/icons/idea00.png",
+        genre: "graphic-design"
+    },
+    {
+        name: "Inclusion Ambassadors",
+        url: "/projects/inclusion-ambassadors.html",
+        iconUrl: "/images/icons/ia00.png",
+        genre: "graphic-design"
+    },
+    {
+        name: "Swift to Hear",
+        url: "/projects/swift-to-hear.html",
+        iconUrl: "/images/icons/swift00.png",
+        genre: "graphic-design"
+    },
+    {
+        name: "IA Logo",
+        url: "/projects/ia-logo.html",
+        iconUrl: "/images/icons/logo00.png",
+        genre: "graphic-design"
+    },
+    {
+        name: "Clockbeat",
+        url: "/projects/clockbeat.html",
+        iconUrl: "/images/icons/clockbeat00.jpg",
+        genre: "installation"
+    },
+    {
+        name: "Hole in the Ground",
+        url: "/projects/hole-in-the-ground.html",
+        iconUrl: "/images/icons/hole00.jpg",
+        genre: "animation"
+    },
+    {
+        name: "Shower Bed",
+        url: "/projects/shower-bed.html",
+        iconUrl: "/images/icons/shower00.jpg",
+        genre: "animation"
+    },
+    {
+        name: "Bouncy Cylinder",
+        url: "/projects/bouncy-cylinder.html",
+        iconUrl: "/images/icons/bouncy00.jpg",
+        genre: "animation"
+    },
+    {
+        name: "This website",
+        url: "/projects/this-website.html",
+        iconUrl: "/images/icons/website00.png",
+        genre: "coding"
+    },
+    {
+        name: "Corprio web app",
+        url: "/projects/corprio-web-app.html",
+        iconUrl: "/images/icons/corprioweb00.png",
+        genre: "coding"
+    },
+    {
+        name: "Corprio app icons",
+        url: "/projects/corprio-app-icons.html",
+        iconUrl: "/images/icons/corprioicons00.png",
+        genre: "graphic-design"
+    }
 ];
 
 //renders div of a single project
@@ -203,13 +248,13 @@ function Page(props) {
     let toBeRendered = [];
 
     projects.forEach(project => {
-        if (project[3] === props.genre || (props.genre === "all" && props.genreExclude != project[3])) {
-            if (!props.exception || project[0] != props.exception[0]) {
+        if (project.genre === props.genre || (props.genre === "all" && props.genreExclude != project.genre)) {
+            if (!props.exception || project.name != props.exception.name) {
                 toBeRendered.push(
                     <Project
-                        value={project[0]}
-                        link={project[1]}
-                        image={project[2]}
+                        value={project.name}
+                        link={project.url}
+                        image={project.iconUrl}
                     />
                 );
             }
@@ -249,13 +294,13 @@ if (currentPage == "/" || currentPage == "/index.html") {
     //is a project page
     if (!isGenrePage) {
         projects.forEach(project => {
-            if (currentPage == project[1]) {
-                toBeUnderlined = "nav-" + project[3];
+            if (currentPage == project.url) {
+                toBeUnderlined = "nav-" + project.genre;
                 //appends other works of the same genre beneath the current project            
-                ReactDOM.render(<Page genre={project[3]} exception={project} />, document.querySelector(".other-works"));
+                ReactDOM.render(<Page genre={project.genre} exception={project} />, document.querySelector(".other-works"));
 
                 //title "Other works in the same genre"
-                const currentGenre = project[3].replaceAll('-', ' ');
+                const currentGenre = project.genre.replaceAll('-', ' ');
                 const text = <h2>Other works in {currentGenre}</h2>
                 ReactDOM.render(text, document.querySelector(".other-works-title"));
             }
